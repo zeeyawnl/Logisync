00fbee6f45f0e2a9b730bf781bbe05174a14de89
ziya <notzeeya@gmail.com>
2025-10-15 18:06:10 +0530

    add stack

--- app/api/auth/callback/stack/route.tsx
+++ app/api/auth/callback/stack/route.tsx
@@ -1,17 +1,19 @@
 import { StackHandler } from '@stackframe/stack';
 import { stackServerApp } from '@/stack/server';
+import { NextRequest } from 'next/server'; // optional if you want request typing
 
-export default function Handler(props: { params: any; searchParams: any }) {
+export default function Handler(props: { params: Record<string, string>; searchParams: URLSearchParams }) {
   return (
     <StackHandler
       app={stackServerApp}
       routeProps={props}
       fullPage={true}
       componentProps={{
-        SignIn: { /* SignIn component props */ },
-        SignUp: { /* SignUp component props */ },
+        SignIn: {},
+        SignUp: {},
         // ... other component props
       }}
     />
   );
 }
+
